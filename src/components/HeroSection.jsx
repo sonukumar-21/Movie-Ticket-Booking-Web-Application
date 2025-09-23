3cd2ca2fcb9c89959781b3a2459718f30087fae1
Sonu Kumar <sonukumar21off@gmail.com>
2025-09-23 16:37:04 +0530

    Navbar updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,11 @@
-import { CalendarIcon, ClockIcon } from "lucide-react";
+import { ArrowRight, CalendarIcon, ClockIcon } from "lucide-react";
 import React from "react";
 import { assets } from "../assets/assets";
 import backgroundImage from "../assets/backgroundImage.png";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+  const navigate = useNavigate();
   return (
     <div
       className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 h-screen bg-cover bg-center"
@@ -28,6 +30,18 @@ const HeroSection = () => {
           <ClockIcon className="w-4.5 h-4.5" /> 2h 8m
         </div>
       </div>
+      <p className="max-w-md text-gray-300">
+        In a post-apocalyptic world where cities ride on wheels and consume each
+        other to survive, two people meet in london and try to stop a
+        conspiracy.
+      </p>
+      <button
+        onClick={() => navigate("/movies")}
+        className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+      >
+        Explore Movies
+        <ArrowRight ClassName="w-5 h-5" />
+      </button>
     </div>
   );
 };
