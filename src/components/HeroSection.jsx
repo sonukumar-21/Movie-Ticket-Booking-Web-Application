0c6a1477c6bf8ce8ebd05c13c5680650a341bf86
Sonu Kumar <sonukumar21off@gmail.com>
2025-09-23 17:08:35 +0530

    updated navbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -16,31 +16,35 @@ const HeroSection = () => {
         alt="Marvel Logo"
         className="max-h-11 lg:h-11 mt-20"
       />
+
       <h1 className="text-5xl md:text-[70px] md:leading-[70px] font-semibold max-w-[700px]">
         Guardians
         <br />
         of the Galaxy
       </h1>
+
       <div className="flex items-center gap-4 text-gray-300">
         <span>Action | Adventure | Sci-Fi</span>
         <div className="flex items-center gap-1">
-          <CalendarIcon className="w-4.5 h-4.5" /> 2018
+          <CalendarIcon className="w-4 h-4" /> 2018
         </div>
         <div className="flex items-center gap-1">
-          <ClockIcon className="w-4.5 h-4.5" /> 2h 8m
+          <ClockIcon className="w-4 h-4" /> 2h 8m
         </div>
       </div>
+
       <p className="max-w-md text-gray-300">
         In a post-apocalyptic world where cities ride on wheels and consume each
-        other to survive, two people meet in london and try to stop a
+        other to survive, two people meet in London and try to stop a
         conspiracy.
       </p>
+
       <button
         onClick={() => navigate("/movies")}
-        className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+        className="flex items-center gap-2 px-6 py-3 text-sm bg-red-500 hover:bg-red-600 transition rounded-full font-medium cursor-pointer text-white"
       >
         Explore Movies
-        <ArrowRight ClassName="w-5 h-5" />
+        <ArrowRight className="w-5 h-5" />
       </button>
     </div>
   );
