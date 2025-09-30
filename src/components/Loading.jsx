21b16dbe1ed5b6585c79d22dedc411792914a359
Sonu Kumar <sonukumar21off@gmail.com>
2025-09-30 16:57:58 +0530

    updation in loading animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-[80vh]">
-      <div
-        className="animate-spin rounded-full h-14 w-14 border-2
-        border-t-primary"
-      ></div>
+    <div className="flex flex-col items-center justify-center h-[80vh] gap-6">
+      {/* Spinner */}
+      <div className="w-16 h-16 border-4 border-t-transparent border-red-600 rounded-full animate-spin shadow-[0_0_20px_rgba(255,0,0,0.5)]"></div>
+
+      {/*  Loading Text */}
+      <p className="text-white text-lg font-medium tracking-wide animate-pulse">
+        Loading...
+      </p>
     </div>
   );
 };
