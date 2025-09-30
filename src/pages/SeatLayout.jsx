b3a0e7bc9377da8d6797f3109d348f138c5e58a9
Sonu Kumar <sonukumar21off@gmail.com>
2025-09-30 17:53:44 +0530

    updated

--- src/pages/SeatLayout.jsx
+++ src/pages/SeatLayout.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
+import Loading from "../components/Loading";
 
 const SeatLayout = () => {
-  return <div></div>;
+  const { id, date } = useParams();
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [show, setShow] = useState(null);
+  const navigate = useNavigate();
+
+  const getShow = async () => {
+    const show = dummyShowsData.find((show) => show._id === id);
+    if (show) {
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getShow();
+  }, []);
+
+  return show ? <div></div> : <Loading />;
 };
 
 export default SeatLayout;
