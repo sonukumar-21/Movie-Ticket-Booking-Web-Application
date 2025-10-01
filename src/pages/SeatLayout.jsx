74fc5e88b647a97c8a8188b37133bf83855f7ca4
Sonu Kumar <sonukumar21off@gmail.com>
2025-10-01 19:59:48 +0530

    Added Available Timing

--- src/pages/SeatLayout.jsx
+++ src/pages/SeatLayout.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import Loading from "../components/Loading";
+import { ClockIcon } from "lucide-react";
 
 const SeatLayout = () => {
   const { id, date } = useParams();
@@ -24,7 +25,30 @@ const SeatLayout = () => {
     getShow();
   }, []);
 
-  return show ? <div></div> : <Loading />;
+  return show ? (
+    <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50">
+      <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:top-30">
+        <p className="text-lg font-semibold px-6">Available Timings</p>
+        <div>
+          {show.dateTime[date].map((item) => (
+            <div
+              className={`flex items-center gap-2 px-6 py-2
+        w-max rounded-r-md cursor-pointer transition $
+        {selectedTime?.time === item.time ? "bg-primary
+        text-white" : "hover:bg-primary/20"}`}
+            >
+              <ClockIcon className="w-4 h-4" />
+              <p className="text-sm">{item.time}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default SeatLayout;
